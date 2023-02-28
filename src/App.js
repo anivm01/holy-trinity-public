45845a704e5c275694e1032df1bc3323d0f7e2e7
anivm01 <anivm.webdev@gmail.com>
2023-02-28 15:30:57 -0500

    remove react boilerplate, add custom favicon and title, install sass, axios and browser router and set up router boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route to='/' />
+        <Route to='/about' />
+        <Route to='/contact-us' />
+        <Route to='/in-memoriam' />
+        <Route to='/community-news' />
+        <Route to='/events' />
+        <Route to='/worship-offices' />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
