abcec16339406568ee15338f0b32b8428ff7986c
anivm01 <anivm.webdev@gmail.com>
2023-05-27 13:24:39 -0400

    reduce hero size

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Hero.scss";
 
-function Hero({image, title}) {
+function Hero({ image, title }) {
   return (
     <section className="hero">
-      <img className="hero__image" src={image} alt="hero" />
+      <div className="hero__background">
+        <img className="hero__image" src={image} alt="hero" />
+      </div>
       <h2 className="hero__title">{title}</h2>
     </section>
   );
