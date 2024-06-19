c620a47e89e6a6d12b794961de6c5aacac379dfb
anivm01 <anivm.webdev@gmail.com>
2024-06-19 11:46:17 -0400

    change pages for feasts and broadcasts in the nav

--- src/pages/FeastsPage/FeastsPage.jsx
+++ src/pages/FeastsPage/FeastsPage.jsx
@@ -3,12 +3,20 @@ import { PageTitle } from "../../components/UI";
 import { feastsTitle } from "../../data/pageTitles";
 import { useLanguage } from "../../utils/LanguageContext";
 import { FeastsList } from "../../components/Feasts";
+import Hero from "../../components/Hero/Hero";
+import HeroBg from "../../components/Hero/HeroBg";
+import eventsHero from "../../assets/feasts-hero.jpg";
 
 const FeastsPage = () => {
   const language = useLanguage();
   return (
     <div>
       <PageTitle title={feastsTitle[language]} />
+      {language === "bg" ? (
+        <HeroBg image={eventsHero} title={"Обединете се"} />
+      ) : (
+        <Hero image={eventsHero} title={"Join together"} />
+      )}
       <FeastsList />
     </div>
   );
